79e4a44a9b68befe1e9dc03be45e283d13f66bb2
LeviYan1998 <leviyan1998@gmail.com>
2020-04-22 18:18:12 +0800

    enable git operation

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,14 +1,14 @@
 # aliases
-alias c="clear"
-alias f="fzf"
-alias h="htop"
+alias c='clear'
+alias f='fzf'
+alias h='htop'
 alias ga='git add'
 alias gc='git clone'
 alias gcd1='git clone --depth=1'
 alias gcm='git commit -m'
 alias gs='git status'
 alias gpom='git push origin master'
-alias r="ranger"
-alias s="screenfetch"
+alias r='ranger'
+alias s='screenfetch'
 
 
